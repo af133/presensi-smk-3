6ba200edfe4f74ed5b31917225bfa652473f728a
Andre <af349393@gmail.com>
2026-06-16 21:25:11 +0700

    updat git commit -m updat

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,10 +1,16 @@
-import Alpine from 'alpinejs';
-
 window.Alpine = Alpine;
+
 Alpine.store('sidebar', {
-    open: false,
+    open: window.innerWidth > 768,
+    
     toggle() {
         this.open = !this.open;
+    },
+    closeIfMobile() {
+        if (window.innerWidth <= 768) {
+            this.open = false;
+        }
     }
 });
+
 Alpine.start();
