955ce2fba96b303bb9fc32dcb3be829fe59c5db3
nama <randy.putra.ant@gmail.com>
2026-06-17 20:23:42 +0700

    Penambahan data dummy dan data denah

---
+++ database/migrations/2026_06_16_152123_create_rooms_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('room_code');
+            $table->integer('floor');
+            $table->geometry('coordinates', subtype: 'polygon', srid: 4326);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
